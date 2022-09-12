d3077276950ccb61b765248975816497574baebe
Varnessa28 <vanessaomtehinse@gmail.com>
2022-09-12 16:04:38 -0700

    reverse alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n <= '0' ; n >= '0' ; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
