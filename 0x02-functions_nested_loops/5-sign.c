1f950b78c2637f609ca86c7f6fbabbdf87faf738
Varnessa28 <vanessaomtehinse@gmail.com>
2022-09-14 02:43:37 -0700

    Times table

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,28 @@
 #include "main.h"
 /**
- * _isalpha - main function
+ * print_sign - Print the sign of a number
  *
- * @c: character to compare
+ * @n: type int integer, can be negative or positive
  *
- * Return: 1 if true, 0 if false
- *
+ * Description: print +, 0, or - depending on number, along with return
+ * Return: 1 if +, 0 if 0, and -1 if -
  */
 
-int _isalpha(int c)
+int print_sign(int n)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
 	}
-	return (0);
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
