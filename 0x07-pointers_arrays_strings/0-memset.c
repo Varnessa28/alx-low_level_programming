149b642bcf3ab7500da03840a286078f4c12e905
Varnessa28 <vanessaomtehinse@gmail.com>
2022-09-26 18:30:49 -0700

    master of array

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -18,16 +18,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	/**
-	 * declare an unsigned int
-	 * because we are storing a
-	 * value that will always be
-	 * non-negative (zero or positive)
-	*/
+	
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0 ; i < n ; i++)
 		s[i] = b;
 
-	return (s);
-}
+	return (s);}
